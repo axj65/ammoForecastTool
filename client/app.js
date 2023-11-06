f61bf6da1152870c8644e1de9a40b828c8874521
Ziyi Wang <ziyiwang@wireless-10-148-191-170.public.utexas.edu>
2023-11-06 17:58:39 -0600

    Formatted the login and register page, and connect them to MongoDB for input and query data

---
+++ client/app.js
@@ -0,0 +1,70 @@
+// The below part is for connecting login and registration database 
+
+const express = require("express")
+const collection = require("./mongo.js")
+const cors = require("cors")
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+
+
+app.get("/",cors(),(req,res)=>{
+
+})
+
+
+app.post("/",async(req,res)=>{
+    const{email,password}=req.body
+
+    try{
+        const check=await collection.findOne({email:email})
+
+        if(check){
+            res.json("exist")
+        }
+        else{
+            res.json("notexist")
+        }
+
+    }
+    catch(e){
+        res.json("fail")
+    }
+
+})
+
+
+
+app.post("/register",async(req,res)=>{
+    const{email,password}=req.body
+
+    const data={
+        email:email,
+        password:password
+    }
+
+    try{
+        const check=await collection.findOne({email:email})
+
+        if(check){
+            res.json("exist")
+        }
+        else{
+            res.json("notexist")
+            await collection.insertMany([data])
+        }
+
+    }
+    catch(e){
+        res.json("fail")
+    }
+
+})
+
+app.listen(5050,()=>{
+    console.log("port connected");
+})
+
+// The above part is for connecting login and registration database 
